d2879a8bd69ed147a25218a9ded9405655db7d4f
ajardeen <mnazar87540@gmail.com>
2024-10-31 23:45:56 +0530

    income&expense app created

---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+# Income Expense Tracker
+
+## Manage Your Finances with Ease
+This project provides a user-friendly Income Expense Calculator built with HTML, CSS, and JavaScript. It empowers you to track your financial status by adding, editing, and deleting income and expense entries. 
+![snippet](./assets/incomeand%20expensespreview.png)
+
+
+## Tech Stack
+* HTML: Structures the foundation of the application.
+* CSS: Styles the elements and creates the visual appeal.
+* JavaScript: Drives the functionality, including data manipulation and user interactions.
+
+## Take Control of Your Finances
+The calculator offers several features to help you manage your budget effectively:
+
+* Intuitive Interface: A clean and user-friendly layout simplifies tracking your income and expenses.
+* Detailed Entry Management: Add descriptions and amounts for both income and expense entries.
+* Filter for Clarity: Choose to view all entries, or filter by income or expenses for a focused overview.
+* Dynamic List: New entries automatically update the list, keeping you organized.
+* Financial Snapshot: Stay informed with real-time displays of Total Income, Total Expenses, and Net Balance.
+* Fresh Start Button: Easily clear the input fields when you're ready to start fresh.
+* Data Persistence: Local storage ensures your data remains even after closing the browser.
+* Responsive Design: Enjoy a seamless experience across desktop and mobile devices.
---
+++ script.js
@@ -0,0 +1,182 @@
+const description = document.getElementById("description");
+const amount = document.getElementById("amount");
+const type = document.getElementById("type");
+const addbtn = document.getElementById("add-btn");
+let transactionData = JSON.parse(localStorage.getItem("transactionData")) || [];
+const resetbtn = document.getElementById("reset-btn");
+
+resetbtn.addEventListener("click", () => {
+  description.value = "";
+  amount.value = "";
+  type.value = "expense";
+});
+
+//new transaction add function
+let addTransaction = () => {
+  let descriptionValue = description.value;
+  let amountValue = amount.value;
+  let typeValue = type.value;
+  //validation for not to add empty fields
+  if (!descriptionValue || !amountValue || !typeValue) {
+    alert("Please fill all the fields");
+    return;
+  }
+  console.log(transactionData);
+  transactionData.push({
+    description: descriptionValue,
+    amount: amountValue,
+    type: typeValue,
+  });
+  localStorage.setItem("transactionData", JSON.stringify(transactionData));
+  //clear input fields
+  description.value = "";
+  amount.value = "";
+  type.value = "expense";
+  console.log(transactionData);
+  transactionListUpdate("all");
+  updateAllValues();
+};
+
+//add transaction button event listener
+addbtn.addEventListener("click", addTransaction);
+
+//filter function
+const allbtn = document.querySelector(".all");
+const expensebtn = document.querySelector(".expense");
+const incomebtn = document.querySelector(".income");
+const filterbtn = [allbtn, expensebtn, incomebtn];
+
+filterbtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.checked) {
+      transactionListUpdate(btn.className);
+    }
+  });
+});
+let ListCreatingCode = (transaction,index) => {
+  const tr = document.createElement("tr");
+  tr.className = "transaction-item";
+  // create a td element for the option column
+  const tdOption = document.createElement("td");
+  tdOption.className = "option";
+  //edit button creation
+  const editButton = document.createElement("button");
+  editButton.innerHTML =
+    '<span class="material-symbols-outlined edit-btn"> edit_note </span>';
+  editButton.addEventListener("click", () => editItem(index));
+  //delete button creation
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML =
+    '<span class="material-symbols-outlined delete-btn">delete</span>';
+  deleteButton.addEventListener("click", () => deleteItem(index));
+  //appending edit and delete buttons to tdOption
+  tdOption.appendChild(editButton);
+  tdOption.appendChild(deleteButton);
+  tr.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${transaction.type}</td>
+            <td>${transaction.description}</td>
+            <td>${transaction.amount}</td>
+      </td>
+     `;
+  tr.appendChild(tdOption);
+  transactionList.appendChild(tr);
+};
+const transactionList = document.getElementById("transaction-list");
+let transactionListUpdate = (filterName) => {
+  transactionList.innerHTML = "";
+
+
+  transactionData.filter((transaction, index) => {
+    if (transaction.type === filterName) {
+      ListCreatingCode(transaction,index);
+    } else if (filterName === "all") {
+      ListCreatingCode(transaction,index);
+    }
+  });
+};
+transactionListUpdate("all");
+
+let deleteItem = (index) => {
+  console.log(index);
+  transactionData.splice(index, 1);
+  localStorage.setItem("transactionData", JSON.stringify(transactionData));
+  transactionListUpdate("all");
+  updateAllValues();
+};
+
+let updateItem = (index) => {
+  if (addbtn.textContent === "Update") {
+    transactionData.splice(index, 1);
+
+    transactionData.splice(index, 0, {
+      description: description.value,
+      amount: amount.value,
+      type: type.value,
+    });
+    description.value = "";
+    amount.value = "";
+    type.value = "expense";
+    localStorage.setItem("transactionData", JSON.stringify(transactionData));
+    transactionListUpdate("all");
+    updateAllValues();
+    addbtn.textContent = "Add";
+  }
+  addbtn.removeEventListener("click", updateItem);
+  addbtn.addEventListener("click", addTransaction);
+};
+
+const editItem = (index) => {
+  description.value = transactionData[index].description;
+  amount.value = transactionData[index].amount;
+  type.value = transactionData[index].type;
+  addbtn.textContent = "Update";
+  addbtn.removeEventListener("click", addTransaction);
+  addbtn.addEventListener("click", () => updateItem(index));
+  updateAllValues();
+  // mobile responsive display purpose only
+  const mobileMediaQuery = window.matchMedia("(max-width: 600px)");
+  if(mobileMediaQuery.matches) {
+    console.log("mobile");
+    
+    financialstatus.style.display = "flex";
+    addbtn.addEventListener("click", () => {
+      financialstatus.style.display = "none";
+    });
+  }
+};
+// Represents the total income amount.
+const totalincome = document.getElementById("total-income");
+const totalexpense = document.getElementById("total-expense");
+const netbalance = document.getElementById("net-balance");
+let updateAllValues = () => {
+  let totalIncome = transactionData.reduce((acc, item) => {
+    if (item.type === "income") {
+      return acc + Number(item.amount);
+    }
+    return acc;
+  }, 0);
+
+  let totalExpense = transactionData.reduce((acc, item) => {
+    if (item.type === "expense") {
+      return acc + Number(item.amount);
+    }
+    return acc;
+  }, 0);
+
+  totalincome.textContent = totalIncome;
+  totalexpense.textContent = totalExpense;
+  let Balance = totalIncome - totalExpense;
+  netbalance.textContent = Balance;
+};
+updateAllValues();
+//mobile responsive
+const addcircle = document.querySelector("#addcircle");
+const financialstatus = document.querySelector("#financialstatus");
+const close = document.querySelector("#close");
+addcircle.addEventListener("click", () => {
+  financialstatus.style.display = "flex";
+});
+close.addEventListener("click", () => {
+  financialstatus.style.display = "none";
+});
